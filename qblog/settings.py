bd6554f3978fbab9360dbc33abc75f53275db869
brickfx <252142844@qq.com>
2015-05-27 19:38:28 +0800

    nginx deployment

--- qblog/settings.py
+++ qblog/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '%wt53%&09y@ru9(=+b_!0cgsdp@vbj%*=2oe8nxx#)8^arr5o^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -85,5 +85,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_FILES_DIRS = (os.path.join(BASE_DIR, "static"))
 STATIC_URL = '/static/'
